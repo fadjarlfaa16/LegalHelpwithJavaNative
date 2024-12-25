4c46f762c3c30fa8ac6ba5a6a9bfb11c22fe5547
fikrialbar15 <fikrialbar2@gmail.com>
2024-12-25 17:48:08 +0700

    Menambahkan codingan manipulasi akun di admin dan get email di user

--- src/main/java/com/mycompany/LegalHelpwithJavaNative/LegalHelpwithJavaNative.java
+++ src/main/java/com/mycompany/LegalHelpwithJavaNative/LegalHelpwithJavaNative.java
@@ -11,6 +11,18 @@ package com.mycompany.legalhelpwithjavanative;
 public class LegalHelpwithJavaNative {
 
     public static void main(String[] args) {
-        
+        Admin admin = new Admin();
+        Artikel artikel = new Artikel("id", "Judul", "Penulis", "Konten", "Tanggal");
+        BukuPasal bukuPasal = new BukuPasal("id", "judul", "penulis", "konten", "tanggal");
+        Konsultan konsultan = new Konsultan(10.00);
+        LegalHelpwithJavaNative legalHelp = new LegalHelpwithJavaNative();
+        Pelanggan pelanggan = new Pelanggan();
+        Post post = new Post("id", "penulis", "konten", "tanggal");
+        Transaksi transaksi = new Transaksi("id", "sender", "receiver", 10.00);
+       
+        System.out.println("Semua objek berhasil diinisialisasi!");
+        admin.getListUser();
+        admin.addArtikel();
+        admin.addBukuPasal();
     }
 }
